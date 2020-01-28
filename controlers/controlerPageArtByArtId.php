dd286a99816fd6fa5ecf1e5506437c232fe90096
thibaut <thibaut.gerin@etu.univ-st-etienne.fr>
2020-01-28 16:49:32 +0100

    som modif for display the autor of art

--- controlers/controlerPageArtByArtId.php
+++ controlers/controlerPageArtByArtId.php
@@ -1,17 +1,18 @@
 <?php
+session_start();
 require "../init/autoloader.php";
 
 use Domain\TypeArt;
 use Domain\Art;
-
+use DAO\DAOusers;
 //call for 3link of header
     use DAO\DAOartType;
     use DAO\DAOart;
     $confing = require "../init/config.inc";
     $daoArtType = new DAOartType($confing);
     $daoArtTypes = $daoArtType->getArtType();
-
 //call for 3link of header
+$daouser = new DAOusers($confing);
 
 
 
@@ -21,10 +22,7 @@ $daoart = new DAOart($confing);
 if (!empty($_GET)){
     $id = $_GET["id"];
     $idArt = $daoart->getArtByArt($id);
-
-
     $artTitlePage = $daoArtType->getTitlByid($id);
-
 
     require "../view/templateArt.php";
 }
