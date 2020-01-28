7d1dc04573a7a69a38abcecf1b4aa2b527aa15e4
thibaut <thibaut.gerin@etu.univ-st-etienne.fr>
2020-01-28 16:50:51 +0100

    adding method getUserForOwnArt
    used at art parge to display autor of art

--- classes/DAO/DAOusers.php
+++ classes/DAO/DAOusers.php
@@ -11,15 +11,16 @@ class DAOusers extends DAObase
         parent::__construct($config);
     }
 
-    public function getUserConected($pseudo, $password){
+    public function getUserConected($pseudo, $password)
+    {
         $user = null;
         $req = $this->bdd->prepare("SELECT idUser,firstName,lastName,pseudo,iconLink,password,description,artPratice,levelAdminUser,mail,entreprise,createdAt,idCountry 
                                               FROM `users` 
                                               WHERE pseudo=:pseudo and password=:password");
-        $req->bindParam(":pseudo",$pseudo);
-        $req->bindParam(":password",$password);
-        if($req->execute()){
-            if($userDataConnect = $req->fetch()){
+        $req->bindParam(":pseudo", $pseudo);
+        $req->bindParam(":password", $password);
+        if ($req->execute()) {
+            if ($userDataConnect = $req->fetch()) {
                 $user = new User(
                     $userDataConnect["idUser"],
                     $userDataConnect["firstName"],
@@ -40,58 +41,60 @@ class DAOusers extends DAObase
         $req->closeCursor();
         return $user;
     }
-public function insertUser($user){
-    $req = $this->bdd->prepare("INSERT INTO users(firstName, lastName, pseudo, iconLink, password, description, artPratice, levelAdminUser, mail, entreprise, idCountry)
+
+    public function insertUser($user)
+    {
+        $req = $this->bdd->prepare("INSERT INTO users(firstName, lastName, pseudo, iconLink, password, description, artPratice, levelAdminUser, mail, entreprise, idCountry)
                                 VALUE (:firstName, :lastName, :pseudo, :iconLink, :password, :description, :artPratice, :levelAdminUser, :mail, :entreprise, :idCountry)
                                 ");
-    $req->bindParam(":firstName",$user->firstName);
-    $req->bindParam(":lastName",$user->lastName);
-    $req->bindParam(":pseudo",$user->pseudo);
-    $req->bindParam(":iconLink", $user->iconLink);
-    $req->bindParam(":password",$user->password);
-    $req->bindParam(":description",$user->description);
-    $req->bindParam(":artPratice",$user->artPratice);
-    $req->bindParam(":levelAdminUser",$user->levelAdminUser);
-    $req->bindParam(":mail",$user->mail);
-    $req->bindParam(":entreprise",$user->entreprise);
-    $req->bindParam(":idCountry",$user->idCountry);
+        $req->bindParam(":firstName", $user->firstName);
+        $req->bindParam(":lastName", $user->lastName);
+        $req->bindParam(":pseudo", $user->pseudo);
+        $req->bindParam(":iconLink", $user->iconLink);
+        $req->bindParam(":password", $user->password);
+        $req->bindParam(":description", $user->description);
+        $req->bindParam(":artPratice", $user->artPratice);
+        $req->bindParam(":levelAdminUser", $user->levelAdminUser);
+        $req->bindParam(":mail", $user->mail);
+        $req->bindParam(":entreprise", $user->entreprise);
+        $req->bindParam(":idCountry", $user->idCountry);
 
-    if($req->execute()){
-        $req->closeCursor();
-        return"echo success a new uses are borne";
-    }
-    else{
-        $req->closeCursor();
-        echo "error fail to adding user";
-    }
-}
-
-public function updateUser($user){
-    $req = $this->bdd->prepare("UPDATE users SET firstName = :firstName, lastName = :lastName, pseudo =:pseudo,iconLink = :iconLink ,password = :password, description = :description ,artPratice = :artPratice,levelAdminUser = :levelAdminUser WHERE isUser  = :id");
-    $req->bindParam(":id" ,$user->idUser);
-    $req->bindParam(":firstName", $user->firstName);
-    $req->bindParam(":lastName", $user->lastName);
-    $req->bindParam(":pseudo", $user->pseudo);
-    $req->bindParam(":iconLink", $user->iconLink);
-    $req->bindParam(":password", $user->password);
-    $req->bindParam("artPratice", $user->artPratice);
-    $req->bindParam("levelAdminUser", $user->levelAdminUser);
-    if ($req->execute()){
-        echo 'MAJ user success';
-    }
-    else{
-        echo "FAIL update";
-    }
+        if ($req->execute()) {
+            $req->closeCursor();
+            return "echo success a new uses are borne";
+        } else {
+            $req->closeCursor();
+            echo "error fail to adding user";
+        }
     }
 
-    public function getUserById($id){
+    public function updateUser($user)
+    {
+        $req = $this->bdd->prepare("UPDATE users SET firstName = :firstName, lastName = :lastName, pseudo =:pseudo,iconLink = :iconLink ,password = :password, description = :description ,artPratice = :artPratice,levelAdminUser = :levelAdminUser WHERE isUser  = :id");
+        $req->bindParam(":id", $user->idUser);
+        $req->bindParam(":firstName", $user->firstName);
+        $req->bindParam(":lastName", $user->lastName);
+        $req->bindParam(":pseudo", $user->pseudo);
+        $req->bindParam(":iconLink", $user->iconLink);
+        $req->bindParam(":password", $user->password);
+        $req->bindParam("artPratice", $user->artPratice);
+        $req->bindParam("levelAdminUser", $user->levelAdminUser);
+        if ($req->execute()) {
+            echo 'MAJ user success';
+        } else {
+            echo "FAIL update";
+        }
+    }
+
+    public function getUserById($id)
+    {
         $user = null;
         $req = $this->bdd->prepare("SELECT idUser,firstName,lastName,pseudo,iconLink,password,description,artPratice,levelAdminUser,mail,entreprise,createdAt,idCountry 
                                               FROM `users` 
                                               WHERE idUser = :id ");
-        $req->bindParam(":id",$id);
-        if($req->execute()){
-            if($userDataConnect = $req->fetch()){
+        $req->bindParam(":id", $id);
+        if ($req->execute()) {
+            if ($userDataConnect = $req->fetch()) {
                 $user = new User(
                     $userDataConnect["idUser"],
                     $userDataConnect["firstName"],
@@ -113,4 +116,32 @@ public function updateUser($user){
         return $user;
     }
 
+    public function getUserForOwnArt($id)
+    {
+        $user = null;
+        $req = $this->bdd->prepare("SELECT * FROM users
+                                    INNER JOIN art ON users.idUser = art.idUser
+                                    WHERE art.idUser = :id");
+        $req->bindParam(":id", $id);
+        if ($req->execute()) {
+            if ($userDataConnect = $req->fetch()) {
+                $user = new User(
+                    $userDataConnect["idUser"],
+                    $userDataConnect["firstName"],
+                    $userDataConnect["lastName"],
+                    $userDataConnect["pseudo"],
+                    $userDataConnect["iconLink"],
+                    $userDataConnect["password"],
+                    $userDataConnect["description"],
+                    $userDataConnect["artPratice"],
+                    $userDataConnect["levelAdminUser"],
+                    $userDataConnect["mail"],
+                    $userDataConnect["entreprise"],
+                    $userDataConnect["createdAt"],
+                    $userDataConnect["idCountry"]);
+            }
+        }
+        $req->closeCursor();
+        return $user;
+    }
 }
