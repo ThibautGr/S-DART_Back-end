40aae9ec9db349c7d3c0421e97df3bddb9337bb5
Thibaut <thibaut.gerin@hotmail.fr>
2020-01-30 21:36:28 +0100

    create method deleteArtById
    unfonctionel need to be fixe

--- classes/DAO/DAOart.php
+++ classes/DAO/DAOart.php
@@ -75,4 +75,28 @@ class DAOart extends DAObase
         $req->closeCursor();
         return $allArt;
     }
+
+    public function deleteArtById($id){
+        $result ="";
+         $query = $this->bdd->prepare("DELETE FROM art WHERE idArt = :id");
+        $query->bindParam(":id", $id);
+        if($query->execute()){
+            echo "element was delete";
+        }
+        else{
+            echo 'delete fail';
+        }
+
+        // Modifie les id de la table en partant de 1.
+        $req = $this->bdd->prepare('SET @count = 0;
+        UPDATE art SET art.idArt = @count:= @count + 1;');
+        $req->execute();
+
+        // Redéfinit l'auto incrémentation de la table.
+        $req = $this->bdd->prepare('ALTER TABLE art AUTO_INCREMENT = 1;');
+        $req->execute();
+
+        return $result;
+
+    }
 }
